60054bd5faab8d37ff845dcf8de94d81084f19bd
Sagiv ben giat <sagiv.bengiat@gmail.com>
2017-06-01 10:05:23 +0300

    added files

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # lessc-glob
 A node package that compiles **ALL** *.less* files in a directory and copy the entire structure to another directory.
+
+This module takes a glob pattern and a target directory.
+Its parsing **ALL** *.less* files in that given pattern to *.css*,
+puts the CSS files into the specified traget directory (as *.css* files) while keeping the same structure of directories.
+
+This function ***does*** run recursively, that is, it *does* look inside subdirectories (with the help of *glob*).
+
+## Installation
+
+    $ npm install lessc-glob
+
+## Usage
+
+### Command Line
+
+    $ lessc-glob  ‹glob›  ‹target›
+
+Where
+`‹glob›` is the glob pattern eg: `./src/**/*.less`.
+`‹target›` is the destination directory for output files eg: `lib`.
+If `‹target›` does not exist, it will automatically be created.
+**Both directories *must* be relative to the current path of the command line.**
+
+### Programmatic
+
+(in your own node module)
+
+Currently not supported.
+
+```js
+// maybe in the future...
+var lesscGlob = require('lessc-glob')
+lesscGlob([glob], [target])
+```
+
+
+## Info
+
+Required
+
+1. Make sure you pass only `.less` files in your glob.
+
+
+## Changelog
+
+It’s on [Github](https://github.com/sag1v/lessc-glob/releases).
---
+++ index.js
@@ -0,0 +1,76 @@
+var glob = require("glob");
+var path = require('path');
+var fs = require('fs');
+var cpx = require('cpx');
+var lessCssStream = require('less-css-stream');
+var chalk = require('chalk');
+
+// log functions
+var chkErr = chalk.bold.red;
+var chkDim = chalk.gray;
+var chkInfo = chalk.cyan;
+var chkMessage = chalk.magenta;
+var chkSuccess = chalk.green;
+var log = console.log;
+
+// args normalization
+var src_glob = path.normalize(process.cwd() + '/' + process.argv[2]) // a glob pattern eg: ./src/**/*.less
+var dir_out = path.normalize(process.cwd() + '/' + process.argv[3]) // directory to put output css eg: lib
+
+// options for cpx, used for the Less to Css stram transform
+var cpxOptions = {
+    transform: function (file) {
+        return lessCssStream(file)
+               .pipe(log(chkInfo(`Parsing - `) + chkDim(`${file}`)));
+    }
+}
+
+log(chkMessage(`process started at ${new Date().toLocaleTimeString()}`));
+log(chkDim(`source glob - ${src_glob}`));
+log(chkDim(`destination - ${dir_out}`));
+log(chkDim('--------------------------------------------------------------'));
+
+/* 
+cpx will copy the whole structure as is,
+while passing it through a transformer stram (less to css stream) and parsing each file to css.
+this didn't changed the .less extension, we will change it later on.
+*/
+cpx.copy(src_glob, dir_out, cpxOptions, function () {
+    // output dir with the glob filter
+    log(chkMessage("Copying files..."));
+    var out_dir_glob = `${dir_out}/**/*.less`;
+
+    glob(out_dir_glob, function (err, files) {
+        if (err) throw err;
+
+        // map on each file and change the extetension
+        files.forEach(function (file) {
+            log(chkInfo(`Renaming: `) + chkDim(`${file}`));
+
+            var newFile = replaceExt(file, '.css');
+            try {
+                fs.renameSync(file, newFile);
+                log(chkSuccess(`Success`) + ': ' + `${chkDim(newFile)}`);
+            }
+            catch (er) {
+                log(chkErr(`Failed to rename ${file} to ${newFile} - ${err}`));
+            }
+            log(chkDim.bold('|>-<|'));
+        });
+        log(chkMessage(`Process ended at ${new Date().toLocaleTimeString()}`));
+    });
+});
+
+
+function replaceExt(npath, ext) {
+    if (typeof npath !== 'string') {
+        return npath;
+    }
+
+    if (npath.length === 0) {
+        return npath;
+    }
+
+    var nFileName = path.basename(npath, path.extname(npath)) + ext;
+    return path.join(path.dirname(npath), nFileName);
+}
